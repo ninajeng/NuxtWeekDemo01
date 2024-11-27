0cd5a733b577dbad505337f86cfe1eed3288413a
nina <ninaw2306@gmail.com>
2024-11-27 15:33:07 +0800

    chore: 修改 baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
-    baseURL: "/NuxtWeekDemo01/",
+    baseURL: "/",
     head: {
       htmlAttrs: {
         lang: "zh-Hant-TW",
