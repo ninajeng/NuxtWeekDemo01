ca80181ae5f352f2508a106297a7ee35b9cb8567
nina <ninaw2306@gmail.com>
2024-11-27 14:24:34 +0800

    feat: 新增帳號頁面與相關元件

---
+++ pages/account.vue
@@ -0,0 +1,16 @@
+<script setup>
+definePageMeta({
+  layout: 'account'
+})
+const route = useRoute();
+
+if(route.fullPath === '/account'){
+  await navigateTo('/account/login');
+}
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style scoped></style>
