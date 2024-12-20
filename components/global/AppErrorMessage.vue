01a400da1dfbe42df439f8239e8e180ce8fbfe7c
nina <ninaw2306@gmail.com>
2024-12-20 10:09:55 +0800

    feat: 新增錯誤訊息元件

---
+++ components/global/AppErrorMessage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+  <div class="text-gray text-center my-5">
+    <Icon class="fs-4 mb-3" icon="ic:baseline-warning" />
+    <p class="fs-5">資料無法載入，請稍後再試！</p>
+  </div>
+</template>
+
+<style scoped>
+.text-gray {
+  color: rgb(128, 128, 128);
+}
+</style>
