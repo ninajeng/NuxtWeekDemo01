a5b97ccd59319185d4f7a0c4d3ce15184c1cef72
nina <ninaw2306@gmail.com>
2024-12-22 22:58:27 +0800

    feat: 新增 middleware，檢查登入狀態

---
+++ middleware/authCheck.global.ts
@@ -0,0 +1,28 @@
+import { checkLogin } from "@/api/checkLogin";
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log(to.name);
+  const redirectPath = "/account/login";
+  const config = useRuntimeConfig();
+  const cookieToken = useCookie(config.public.cookieToken);
+  const token = cookieToken.value;
+
+  const isNeedAuth = [
+    "user-userId-profile",
+    "user-userId-order",
+    "rooms-roomId-booking",
+  ];
+
+  if (isNeedAuth.includes(to.name as string)) {
+    if (!token) {
+      return navigateTo(redirectPath);
+    }
+
+    const { data, error } = await checkLogin(token);
+    const response = data as { value: { status: boolean } };
+    if (error.value || !response.value?.status) {
+      cookieToken.value = "";
+      return navigateTo(redirectPath);
+    }
+  }
+});
