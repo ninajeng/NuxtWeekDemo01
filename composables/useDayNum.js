086574131f9dfa3f51224551b374003a50562bf6
nina <ninaw2306@gmail.com>
2024-12-23 18:54:35 +0800

    fix: 修正引數命名

--- composables/useDayNum.js
+++ composables/useDayNum.js
@@ -1,8 +1,8 @@
 export const useDayNum = () => {
-  const countDay = (endDate, startDay) => {
-    if (!endDate || !startDay) return;
+  const countDay = (startDate, endDate) => {
+    if (!startDate || !endDate) return;
     const end = new Date(endDate);
-    const start = new Date(startDay);
+    const start = new Date(startDate);
     return parseInt(Math.abs(end - start) / 1000 / 60 / 60 / 24);
   };
 
