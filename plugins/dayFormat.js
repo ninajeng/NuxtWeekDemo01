a92379de1fef22e76f3483837f1d7f6498a39af0
nina <ninaw2306@gmail.com>
2024-12-23 14:51:40 +0800

    chore: 安裝 dayjs 套件並新增 plugins

---
+++ plugins/dayFormat.js
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive("dayFormat", {
+    mounted(el, binding) {
+      const dayAppendix = ["日", "一", "二", "三", "四", "五", "六"];
+      const dayjsObj = dayjs(binding.value);
+      const day = dayAppendix[dayjsObj.day()];
+      el.textContent = dayjsObj.format(`M月D日 星期${day}`);
+    },
+  });
+});
